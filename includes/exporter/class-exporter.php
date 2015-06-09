f361265e2d12c9c6aeea50cbca534b4ee5c06251
Federico Ramirez <federico_r@beezwax.net>
2015-06-09 15:59:28 -0300

    Implement video component

--- includes/exporter/class-exporter.php
+++ includes/exporter/class-exporter.php
@@ -227,6 +227,8 @@ class Exporter {
 				$component = $this->create_component_or_null( $image_node );
 			} else if ( $ewv = $this->node_contains( $node, 'iframe' ) ) {
 				$component = $this->create_component_or_null( $ewv );
+			} else if ( $video = $this->node_contains( $node, 'video' ) ) {
+				$component = $this->create_component_or_null( $video );
 			} else if ( $this->node_contains( $node, 'script' ) ) {
 				// Ignore script tags.
 				$component = null;
