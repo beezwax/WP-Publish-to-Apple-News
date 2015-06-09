fa9217326b0720bd5a9b1f8062a924423fe84185
Federico Ramirez <federico_r@beezwax.net>
2015-06-08 21:45:05 -0300

    Implement quote component

--- includes/exporter/class-component-factory.php
+++ includes/exporter/class-component-factory.php
@@ -4,6 +4,7 @@ namespace Exporter;
 require_once plugin_dir_path( __FILE__ ) . 'components/class-component.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/class-image.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/class-body.php';
+require_once plugin_dir_path( __FILE__ ) . 'components/class-quote.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/class-heading.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/class-embed-web-video.php';
 require_once plugin_dir_path( __FILE__ ) . 'components/class-intro.php';
@@ -18,13 +19,14 @@ class Component_Factory {
 	public static function initialize( $workspace ) {
 		self::$workspace = $workspace;
 
-		self::register_component( 'img'    ,   '\\Exporter\\Components\\Image'           );
-		self::register_component( 'p'      ,   '\\Exporter\\Components\\Body'            );
-		self::register_component( 'h[1-6]' ,   '\\Exporter\\Components\\Heading'         );
-		self::register_component( 'iframe' ,   '\\Exporter\\Components\\Embed_Web_Video' );
-		self::register_component( 'intro'  ,   '\\Exporter\\Components\\Intro'           );
-		self::register_component( 'cover'  ,   '\\Exporter\\Components\\Cover'           );
-		self::register_component( 'gallery',   '\\Exporter\\Components\\Gallery'         );
+		self::register_component( 'img'       ,   '\\Exporter\\Components\\Image'           );
+		self::register_component( 'p'         ,   '\\Exporter\\Components\\Body'            );
+		self::register_component( 'blockquote',   '\\Exporter\\Components\\Quote'           );
+		self::register_component( 'h[1-6]'    ,   '\\Exporter\\Components\\Heading'         );
+		self::register_component( 'iframe'    ,   '\\Exporter\\Components\\Embed_Web_Video' );
+		self::register_component( 'intro'     ,   '\\Exporter\\Components\\Intro'           );
+		self::register_component( 'cover'     ,   '\\Exporter\\Components\\Cover'           );
+		self::register_component( 'gallery'   ,   '\\Exporter\\Components\\Gallery'         );
 	}
 
 	private static function register_component( $tagname, $classname ) {
