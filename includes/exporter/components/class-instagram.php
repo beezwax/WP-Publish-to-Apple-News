fe7f408a5ee33691a7cf29aadb6bac17d383e3a2
Federico Ramirez <federico_r@beezwax.net>
2015-06-10 13:52:04 -0300

    Base matching regex on WordPress' one

--- includes/exporter/components/class-instagram.php
+++ includes/exporter/components/class-instagram.php
@@ -2,14 +2,14 @@
 namespace Exporter\Components;
 
 /**
- * A tweet embed code consists of a blockquote followed by a script tag. Parse
+ * Instagram embed code consists of a blockquote followed by a script tag. Parse
  * the blockquote only and ignore the script tag, as all we need is the URL.
  */
 class Instagram extends Component {
 
 	protected function build( $text ) {
-		// Find tweeter URL in HTML string
-		if( ! preg_match( '/https:\/\/instagram.com\/p\/([^\/]+)\//', $text, $matches ) ) {
+		// Find instagram URL in HTML string
+		if( ! preg_match( '#https?://instagr(\.am|am\.com)/p/([^\/]+)/#', $text, $matches ) ) {
 			return null;
 		}
 
