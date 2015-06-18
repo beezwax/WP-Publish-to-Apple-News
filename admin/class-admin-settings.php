df259e79cf8638e6a986cc2b1097e9890572267c
Federico Ramirez <federico_r@beezwax.net>
2015-06-18 18:14:32 -0300

    Use better field types

--- admin/class-admin-settings.php
+++ admin/class-admin-settings.php
@@ -5,18 +5,69 @@ use Exporter\Settings as Settings;
 
 class Admin_Settings {
 
+	private $field_types = array(
+		'body_size' => 'integer',
+		'body_color' => 'color',
+		'body_link_color' => 'color',
+		'initial_dropcap' => 'boolean',
+		'dropcap_color' => 'color',
+		'header_color' => 'color',
+		'header1_size' => 'integer',
+		'header2_size' => 'integer',
+		'header3_size' => 'integer',
+		'header4_size' => 'integer',
+		'header5_size' => 'integer',
+		'header6_size' => 'integer',
+		'pullquote_size' => 'integer',
+		'pullquote_color' => 'color',
+		'pullquote_transform' => array( 'none', 'uppercase' ),
+		'gallery_type' => array( 'gallery', 'mosaic' ),
+	);
+
 	function __construct() {
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
 		add_action( 'admin_menu', array( $this, 'setup_options_page' ) );
 	}
 
+	private function get_type_for_field( $name ) {
+		if( array_key_exists( $name, $this->field_types ) ) {
+			return $this->field_types[ $name ];
+		}
+
+		return 'string';
+	}
+
 	public function render_field( $args ) {
 		list( $name, $default_value ) = $args;
-		printf(
-			'<input type="text" id="title" name="%s", value="%s">',
-			$name,
-			get_option( $name ) ? esc_attr( get_option( $name ) ) : $default_value
-		);
+		$type  = $this->get_type_for_field( $name );
+		$value = esc_attr( get_option( $name ) ) ?: $default_value;
+		$field = null;
+
+		if ( is_array( $type ) ) {
+			$field = '<select name="%s">';
+			foreach ( $type as $option ) {
+				$field .= "<option value='$option'";
+				if ( $option == $value ) {
+					$field .= ' selected ';
+				}
+				$field .= ">$option</option>";
+			}
+			$field .= '</select>';
+		} else if( 'boolean' == $type ) {
+			$field = '<input type="checkbox"';
+			if ( $value ) {
+				$field .= ' checked ';
+			}
+			$field .= '>';
+		} else if( 'string' == $type ) {
+			$field = '<input type="text" id="title" name="%s", value="%s">';
+		} else if ( 'integer' == $type ) {
+			$field = '<input type="number" id="title" name="%s", value="%s">';
+		} else if ( 'color' == $type ) {
+			$field = '<input type="color" id="title" name="%s", value="%s">';
+	  }
+
+		printf( $field, $name, $value );
 	}
 
 	public function print_section_info() {
