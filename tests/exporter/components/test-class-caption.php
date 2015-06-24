be669b11432b5e694eb62fe4038f1659fa7373e3
Federico Ramirez <federico_r@beezwax.net>
2015-06-24 20:03:15 -0300

    Test for figure element

--- tests/exporter/components/test-class-caption.php
+++ tests/exporter/components/test-class-caption.php
@@ -7,11 +7,15 @@ use \Exporter\Component_Styles as Component_Styles;
 
 class Caption_Test extends PHPUnit_Framework_TestCase {
 
+	public function setup() {
+		$this->settings = new Settings();
+		$this->styles   = new Component_Styles();
+		$this->layouts  = new Component_Layouts();
+	}
+
 	public function testSingleCaption() {
-		$settings  = new Settings();
-		$styles    = new Component_Styles();
-		$layouts   = new Component_Layouts();
-		$component = new Caption( '<figcaption>my text</figcaption>', null, $settings, $styles, $layouts );
+		$component = new Caption( '<figcaption>my text</figcaption>', null,
+			$this->settings, $this->styles, $this->layouts );
 
 		$this->assertEquals(
 			array(
@@ -20,6 +24,23 @@ class Caption_Test extends PHPUnit_Framework_TestCase {
 		 	),
 			$component->value()
 		);
+	}
+
+	private function build_node( $html ) {
+		$dom = new DOMDocument( '1.0', 'utf-8' );
+		libxml_use_internal_errors( true );
+		$dom->loadHTML( $html );
+		libxml_clear_errors( true );
+		return $dom->getElementsByTagName( 'body' )->item( 0 )->childNodes->item( 0 );
+	}
+
+	public function testCompoundCaption() {
+		$node = $this->build_node( '<figure><img src="example.jpg"/><figcaption>my text</figcaption></figure>' );
+		$result = Caption::node_matches( $node );
+
+		// Matched two elements, by shortname (img and caption).
+		$this->assertTrue( array_key_exists( 'img', $result ) );
+		$this->assertTrue( array_key_exists( 'caption', $result ) );
 	}
 
 }
