b8e951796d169cd6fb43fb30c8c5a31662009bf3
Federico Ramirez <federico_r@beezwax.net>
2015-07-05 18:32:03 -0300

    Formatting

--- tests/exporter/builders/test-class-components.php
+++ tests/exporter/builders/test-class-components.php
@@ -17,10 +17,7 @@ class Component_Tests extends PHPUnit_Framework_TestCase {
 		$this->content  = new Exporter_Content( 1, 'My Title', '<p>Hello, World!</p>' );
 		$this->styles   = new Component_Text_Styles( $this->content, $this->settings );
 		$this->layouts  = new Component_Layouts( $this->content, $this->settings );
-
-		$workspace = $this->prophet->prophesize( '\Exporter\Workspace' );
-		//$workspace->get_file_contents( 'http://someurl.com/audio-file.mp3?some_query=string' )->willReturn( 'foo' )->shouldBeCalled();
-		//$workspace->write_tmp_file( 'audio-file.mp3', 'foo' )->willReturn( true )->shouldBeCalled();
+		$workspace      = $this->prophet->prophesize( '\Exporter\Workspace' );
 
 		Component_Factory::initialize( $workspace, $this->settings, $this->styles, $this->layouts );
 	}
@@ -33,20 +30,9 @@ class Component_Tests extends PHPUnit_Framework_TestCase {
 		$builder = new Components( $this->content, $this->settings );
 		$result  = $builder->to_array();
 
-		$this->assertEquals(
-			2,
-			count( $result )
-		);
-
-		$this->assertEquals(
-			'title',
-			$result[0]['role']
-		);
-
-		$this->assertEquals(
-			'body',
-			$result[1]['role']
-		);
+		$this->assertEquals( 2, count( $result ) );
+		$this->assertEquals( 'title', $result[0]['role'] );
+		$this->assertEquals( 'body', $result[1]['role'] );
 	}
 
 }
