b6f2e764e1cb3855d768c2d1ff4a5aedd9b663e7
Federico Ramirez <federico_r@beezwax.net>
2015-07-06 14:22:21 -0300

    Conventions

--- includes/exporter/builders/class-metadata.php
+++ includes/exporter/builders/class-metadata.php
@@ -12,14 +12,14 @@ class Metadata extends Builder {
 		// The content's intro is optional. In WordPress, it's a post's
 		// excerpt. It's an introduction to the article.
 		if ( $this->content_intro() ) {
-			$meta[ 'excerpt' ] = $this->content_intro();
+			$meta['excerpt'] = $this->content_intro();
 		}
 
 		// If the content has a cover, use it as thumb.
 		if ( $this->content_cover() ) {
 			$filename  = basename( $this->content_cover() );
 			$thumb_url = 'bundle://' . $filename;
-			$meta[ 'thumbnailURL' ] = $thumb_url;
+			$meta['thumbnailURL'] = $thumb_url;
 		}
 
 		return $meta;
