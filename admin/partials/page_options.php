f490982fbc32336fc11b5b9c18843b9a5d8e5544
Federico Ramirez <federico_r@beezwax.net>
2015-07-14 15:11:58 -0300

    Update settings labels

--- admin/partials/page_options.php
+++ admin/partials/page_options.php
@@ -1,28 +1,9 @@
 <div class="wrap">
-	<h1>Apple Export Options</h1>
+	<h1>Apple News Options</h1>
 
 	<form method="post" action="options.php">
 		<?php settings_fields( 'apple-export-options' ); ?>
 		<?php do_settings_sections( 'apple-export-options' ); ?>
-
-		<!--
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row">Body Font</th>
-				<td>
-					<input type="text" name="body_font" value="<?php echo esc_attr( get_option( 'body_font' ) ); ?>">
-				</td>
-			</tr>
-
-			<tr valign="top">
-				<th scope="row">Body Size</th>
-				<td>
-					<input type="text" name="body_size" value="<?php echo esc_attr( get_option( 'body_size' ) ); ?>">
-				</td>
-			</tr>
-		</table>
-		-->
-
 		<?php submit_button(); ?>
 	</form>
 </div>
