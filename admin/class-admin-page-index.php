8b207b5c3280e4a71dbd77494cb0b6ee33e7350c
Federico Ramirez <federico_r@beezwax.net>
2015-07-15 19:08:03 -0300

    Remove debug code

--- admin/class-admin-page-index.php
+++ admin/class-admin-page-index.php
@@ -128,8 +128,7 @@ class Admin_Page_Index extends Apple_Export {
 	private function export_action( $id ) {
 		$action = new Actions\Index\Export( $this->settings, $id );
 		$path   = $action->perform();
-		//$this->download_zipfile( $path );
-		echo $path;
+		$this->download_zipfile( $path );
 	}
 
 	private function push_action( $id ) {
