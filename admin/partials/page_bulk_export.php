0940eaafa640eb5ddef5fdafb39c4ec9fb704c53
bcampeau <brad@alleyinteractive.com>
2015-08-31 19:59:14 -0400

    Added better sanitization, fixed back button

--- admin/partials/page_bulk_export.php
+++ admin/partials/page_bulk_export.php
@@ -15,6 +15,6 @@
 		<?php endforeach; ?>
 	</ul>
 
-	<a class="button" href="<?php echo esc_url( menu_page_url( $this->plugin_slug . '_index', true ) ) ?>"><?php esc_html_e( 'Back', 'apple-news' ) ?></a>
+	<a class="button" href="<?php echo esc_url( menu_page_url( $this->plugin_slug . '_index', false ) ) ?>"><?php esc_html_e( 'Back', 'apple-news' ) ?></a>
 	<a class="button button-primary bulk-export-submit" href="#"><?php esc_html_e( 'Publish All', 'apple-news' ) ?></a>
 </div>
