8048d1bc525631d8f8a8f3e9dd6279ce172bdea5
bcampeau <brad@alleyinteractive.com>
2015-09-01 14:48:32 -0400

    Spacing this

--- assets/js/export-table.js
+++ assets/js/export-table.js
@@ -3,15 +3,15 @@
 
 	$( '.share-url-button' ).click(function ( e ) {
 		e.preventDefault();
-		$(this).siblings( '.apple-share-url' ).toggle();
+		$( this ).siblings( '.apple-share-url' ).toggle();
 	});
 
 	$( '.row-actions' ).mouseenter (function () {
-		$(this).addClass( 'is-active' );
+		$( this ).addClass( 'is-active' );
 	});
 
 	$( '.row-actions' ).mouseleave(function () {
-		$(this).removeClass( 'is-active' );
+		$( this ).removeClass( 'is-active' );
 	});
 
 })( jQuery, window );
