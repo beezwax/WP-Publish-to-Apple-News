27f54b11cee89a036ab89ec87a5602e0f957fa84
bcampeau <brad@alleyinteractive.com>
2015-09-08 12:42:57 +0200

    Added capabilities check to ajax action

--- admin/class-admin-apple-bulk-export-page.php
+++ admin/class-admin-apple-bulk-export-page.php
@@ -84,6 +84,15 @@ class Admin_Apple_Bulk_Export_Page extends Apple_News {
 		// Check the nonce
 		check_ajax_referer( self::ACTION );
 
+		// Check capabilities
+		if ( ! current_user_can( apply_filters( 'apple_news_publish_capability', 'manage_options' ) ) ) {
+			echo json_encode( array(
+				'success' => false,
+				'error'   => __( 'You do not have permission to publish to Apple News', 'apple-news' ),
+			) );
+			wp_die();
+		}
+
 		// Sanitize input data
 		$id = absint( $_GET['id'] );
 
