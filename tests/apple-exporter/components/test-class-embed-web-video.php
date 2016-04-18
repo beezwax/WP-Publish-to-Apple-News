2297c29ed9244181e0043885baf740ab88f9029f
bcampeau <brad@alleyinteractive.com>
2016-04-18 19:38:51 -0400

    Improve YouTube unit tests

--- tests/apple-exporter/components/test-class-embed-web-video.php
+++ tests/apple-exporter/components/test-class-embed-web-video.php
@@ -6,7 +6,49 @@ use Apple_Exporter\Components\Embed_Web_Video as Embed_Web_Video;
 
 class Embed_Web_Video_Test extends Component_TestCase {
 
-	public function testYouTubeWatchLink() {
+	public function testHTTPYouTubeWatchLink() {
+		$component = new Embed_Web_Video( '<p>http://youtube.com/watch?v=0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSYouTubeWatchLink() {
+		$component = new Embed_Web_Video( '<p>https://youtube.com/watch?v=0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPWWWYouTubeWatchLink() {
+		$component = new Embed_Web_Video( '<p>http://www.youtube.com/watch?v=0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSWWWYouTubeWatchLink() {
 		$component = new Embed_Web_Video( '<p>https://www.youtube.com/watch?v=0qwALOOvUik</p>',
 			null, $this->settings, $this->styles, $this->layouts );
 
@@ -20,7 +62,49 @@ class Embed_Web_Video_Test extends Component_TestCase {
 		);
 	}
 
-	public function testYouTubeEmbedLink() {
+	public function testHTTPYouTubeEmbedLink() {
+		$component = new Embed_Web_Video( '<p>http://youtube.com/embed/0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSYouTubeEmbedLink() {
+		$component = new Embed_Web_Video( '<p>https://youtube.com/embed/0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPWWWYouTubeEmbedLink() {
+		$component = new Embed_Web_Video( '<p>http://www.youtube.com/embed/0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSWWWYouTubeEmbedLink() {
 		$component = new Embed_Web_Video( '<p>https://www.youtube.com/embed/0qwALOOvUik</p>',
 			null, $this->settings, $this->styles, $this->layouts );
 
@@ -34,7 +118,21 @@ class Embed_Web_Video_Test extends Component_TestCase {
 		);
 	}
 
-	public function testYouTubeShortLink() {
+	public function testHTTPYouTubeShortLink() {
+		$component = new Embed_Web_Video( '<p>http://youtu.be/0qwALOOvUik</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSYouTubeShortLink() {
 		$component = new Embed_Web_Video( '<p>https://youtu.be/0qwALOOvUik</p>',
 			null, $this->settings, $this->styles, $this->layouts );
 
