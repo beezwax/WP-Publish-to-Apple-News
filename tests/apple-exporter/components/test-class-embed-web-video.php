1353a906958461480ee21dc13500115ae6462946
bcampeau <brad@alleyinteractive.com>
2016-04-18 18:48:44 -0700

    Handled all known YouTube and Vimeo embed formats, added many supporting unit tests

--- tests/apple-exporter/components/test-class-embed-web-video.php
+++ tests/apple-exporter/components/test-class-embed-web-video.php
@@ -146,8 +146,134 @@ class Embed_Web_Video_Test extends Component_TestCase {
 		);
 	}
 
-	public function testVimeo() {
+	public function testYouTubeWatchLinkWithHyphen() {
+		$component = new Embed_Web_Video( '<p>https://www.youtube.com/watch?v=J2nN-Yrt1EU</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/J2nN-Yrt1EU',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testYouTubeEmbedLinkWithHyphen() {
+		$component = new Embed_Web_Video( '<p>https://www.youtube.com/embed/J2nN-Yrt1EU</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/J2nN-Yrt1EU',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testYouTubeShortLinkWithHyphen() {
+		$component = new Embed_Web_Video( '<p>https://youtu.be/J2nN-Yrt1EU</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/J2nN-Yrt1EU',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPYouTubeIframe() {
+		$component = new Embed_Web_Video( '<iframe title="YouTube" width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik" frameborder="0" allowfullscreen></iframe>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTTPSYouTubeIframe() {
+		$component = new Embed_Web_Video( '<iframe title="YouTube" width="640" height="360" src="https://www.youtube.com/embed/0qwALOOvUik" frameborder="0" allowfullscreen></iframe>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testYouTubeIframeWithParams() {
+		$component = new Embed_Web_Video( '<iframe width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik?autoplay=1"></iframe>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testSelfClosingIframe() {
+		$component = new Embed_Web_Video( '<iframe width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik" />',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testHTMLFormatFromEmbedShortcode() {
+		$component = new Embed_Web_Video( '<p><a href="http://youtube.com/embed/0qwALOOvUik">http://youtube.com/embed/0qwALOOvUik</a></p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://www.youtube.com/embed/0qwALOOvUik',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testVimeoLink() {
 		$component = new Embed_Web_Video( '<p>https://vimeo.com/12819723</p>',
+			null, $this->settings, $this->styles, $this->layouts );
+
+		$this->assertEquals(
+			array(
+				'role' => 'embedwebvideo',
+				'URL' => 'https://player.vimeo.com/video/12819723',
+				'aspectRatio' => '1.777',
+		 	),
+			$component->to_array()
+		);
+	}
+
+	public function testVimeoIframe() {
+		$component = new Embed_Web_Video( '<iframe src="//player.vimeo.com/video/12819723" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
 			null, $this->settings, $this->styles, $this->layouts );
 
 		$this->assertEquals(
@@ -180,4 +306,3 @@ class Embed_Web_Video_Test extends Component_TestCase {
 	}
 
 }
-
