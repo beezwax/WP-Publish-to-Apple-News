62e952d6b8cd509a726e00119315df621231420a
bcampeau <brad@alleyinteractive.com>
2016-08-26 22:59:09 -0400

    Undoing formatting

--- admin/class-admin-apple-notice.php
+++ admin/class-admin-apple-notice.php
@@ -46,7 +46,7 @@ class Admin_Apple_Notice {
 		}
 
 		self::add_user_meta( $user_id, self::KEY, array(
-			'message' => wp_kses_post( $message ),
+			'message' => sanitize_text_field( $message ),
 			'type' => sanitize_text_field( $type )
 		) );
 	}
