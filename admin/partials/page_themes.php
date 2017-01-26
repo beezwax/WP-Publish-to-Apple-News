594869f1dd566809e8dc72cfa944924134326ed3
bcampeau <brad@alleyinteractive.com>
2017-01-25 21:08:25 -0500

    Fixed formatting

--- admin/partials/page_themes.php
+++ admin/partials/page_themes.php
@@ -8,55 +8,55 @@
 			'secondary',
 			'apple_news_start_create',
 			false
-		 ); ?>
-		 <?php submit_button(
+		); ?>
+		<?php submit_button(
 			__( 'Import Theme', 'apple-news' ),
 			'secondary',
 			'apple_news_start_import',
 			false
-		 ); ?>
+		); ?>
 
-		 <div class="apple-news-theme-form" id="apple_news_new_theme_options">
-		 <b><?php esc_html_e( 'Theme name', 'apple-news' ) ?>:</b>
-		 <input type="text" id="apple_news_theme_name" name="apple_news_theme_name" value="" maxlength="45" />
-		 <?php submit_button(
-			__( 'Save', 'apple-news' ),
-			'primary',
-			'apple_news_create_theme',
-			false
-		 ); ?>
-		 <?php submit_button(
-			__( 'Cancel', 'apple-news' ),
-			'secondary',
-			'apple_news_cancel_create_theme',
-			false
-		 ); ?>
-		 </div>
+		<div class="apple-news-theme-form" id="apple_news_new_theme_options">
+			<b><?php esc_html_e( 'Theme name', 'apple-news' ) ?>:</b>
+			<input type="text" id="apple_news_theme_name" name="apple_news_theme_name" value="" maxlength="45" />
+			<?php submit_button(
+				__( 'Save', 'apple-news' ),
+				'primary',
+				'apple_news_create_theme',
+				false
+			); ?>
+			<?php submit_button(
+				__( 'Cancel', 'apple-news' ),
+				'secondary',
+				'apple_news_cancel_create_theme',
+				false
+			); ?>
+		</div>
 
-		 <div class="apple-news-theme-form" id="apple_news_import_theme">
-		 <p>
-		 <b><?php esc_html_e( 'Choose a file to upload', 'apple-news' ) ?>:</b> <input type="file" id="apple_news_import_file" name="import" size="25" />
-		 <br /><?php esc_html_e( '(max size 1MB)', 'apple-news' ) ?>
-		 </p>
-		 <?php submit_button(
-			__( 'Upload', 'apple-news' ),
-			'primary',
-			'apple_news_upload_theme',
-			false
-		 ); ?>
-		 <?php submit_button(
-			__( 'Cancel', 'apple-news' ),
-			'secondary',
-			'apple_news_cancel_upload_theme',
-			false
-		 ); ?>
-		 <input type="hidden" name="max_file_size" value="1000000" />
-		 </div>
+		<div class="apple-news-theme-form" id="apple_news_import_theme">
+			<p>
+				<b><?php esc_html_e( 'Choose a file to upload', 'apple-news' ) ?>:</b> <input type="file" id="apple_news_import_file" name="import" size="25" />
+				<br /><?php esc_html_e( '(max size 1MB)', 'apple-news' ) ?>
+			</p>
+			<?php submit_button(
+				__( 'Upload', 'apple-news' ),
+				'primary',
+				'apple_news_upload_theme',
+				false
+			); ?>
+			<?php submit_button(
+				__( 'Cancel', 'apple-news' ),
+				'secondary',
+				'apple_news_cancel_upload_theme',
+				false
+			); ?>
+			<input type="hidden" name="max_file_size" value="1000000" />
+		</div>
 
 		<?php submit_button(
 			__( 'Set Theme', 'apple-news' ),
 			'primary',
 			'apple_news_set_theme'
-		 ); ?>
+		); ?>
 	</form>
 </div>
