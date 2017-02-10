3d35e8e01e3641d9fca405d84ac02380eb35847e
bcampeau <brad@alleyinteractive.com>
2017-02-10 14:00:08 -0500

    Created unit test for section map

--- tests/admin/apple-actions/index/test-class-export.php
+++ tests/admin/apple-actions/index/test-class-export.php
@@ -91,7 +91,62 @@ class Admin_Action_Index_Export_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'by #Testuser | Aug 26, 2016 | 12:00 PM', $exporter_content->byline() );
 	}
 
-	// TODO: add tests for mappings
+	public function testSectionMapping() {
+		// Create a post
+		$title = 'My Title';
+		$content = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique quis justo sit amet eleifend. Praesent id metus semper, fermentum nibh at, malesuada enim. Mauris eget faucibus lectus. Vivamus iaculis eget urna non porttitor. Donec in dignissim neque. Vivamus ut ornare magna. Nulla eros nisi, maximus nec neque at, condimentum lobortis leo. Fusce in augue arcu. Curabitur lacus elit, venenatis a laoreet sit amet, imperdiet ac lorem. Curabitur sed leo sed ligula tempor feugiat. Cras in tellus et elit volutpat.</p>';
+
+		$post_id = $this->factory->post->create( array(
+			'post_title' => $title,
+			'post_content' => $content,
+		) );
+
+		// Create a term and add it to the post
+		$term_id = $this->factory->term->create( array(
+			'taxonomy' => 'category',
+			'name' => 'news',
+		) );
+		wp_set_post_terms( $post_id, array( $term_id ), 'category' );
+
+		// Create a taxonomy map
+		update_option( \Admin_Apple_Sections::TAXONOMY_MAPPING_KEY, array(
+			'abcdef01-2345-6789-abcd-ef012356789a' => array( $term_id ),
+		) );
+
+		// Cache as a transient to bypass the API call
+		$self = 'https://u48r14.digitalhub.com/channels/abcdef01-2345-6789-abcd-ef012356789a';
+		set_transient(
+			'apple_news_sections',
+			array(
+				(object) array(
+					'createdAt' => '2017-01-01T00:00:00Z',
+					'id' => 'abcdef01-2345-6789-abcd-ef012356789a',
+					'isDefault' => true,
+					'links' => (object) array(
+						'channel' => 'https://u48r14.digitalhub.com/channels/abcdef01-2345-6789-abcd-ef0123567890',
+						'self' => $self,
+					),
+					'modifiedAt' => '2017-01-01T00:00:00Z',
+					'name' => 'Main',
+					'shareUrl' => 'https://apple.news/AbCdEfGhIj-KlMnOpQrStUv',
+					'type' => 'section',
+				),
+			)
+		);
+
+		// Get sections for the post
+		$sections = \Admin_Apple_Sections::get_sections_for_post( $post_id );
+
+		// Check that the correct mapping was returned
+		$this->assertEquals(
+			$sections,
+			array( $self )
+		);
+
+		// Remove the transient and the map
+		delete_option( \Admin_Apple_Sections::TAXONOMY_MAPPING_KEY );
+		delete_transient( 'apple_news_sections' );
+	}
 
 }
 
