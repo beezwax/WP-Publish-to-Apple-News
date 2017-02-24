d95256b4e4c92d65aea862e1283ce3c62ed426a1
bcampeau <brad@alleyinteractive.com>
2017-02-24 17:41:19 -0500

    Finished unit tests

--- tests/admin/test-class-admin-apple-json.php
+++ tests/admin/test-class-admin-apple-json.php
@@ -1,18 +1,18 @@
 <?php
 
-//use Apple_Exporter\Exporter_Content as Exporter_Content;
 use Apple_Exporter\Settings as Settings;
 use Apple_Exporter\Builders\Component_Layouts as Component_Layouts;
 use Apple_Exporter\Builders\Component_Text_Styles as Component_Text_Styles;
 use Apple_Exporter\Components\Advertisement as Advertisement;
+use Apple_Exporter\Components\Audio as Audio;
 
 class Admin_Apple_JSON_Test extends WP_UnitTestCase {
 
 	public function setup() {
 		parent::setup();
 
+		$this->prophet  = new \Prophecy\Prophet;
 		$this->settings = new Settings();
-//		$this->content  = new Exporter_Content( 1, __( 'My Title', 'apple-news' ), '<p>' . __( 'Hello, World!', 'apple-news' ) . '</p>' );
 		$this->styles   = new Component_Text_Styles( $this->content, $this->settings );
 		$this->layouts  = new Component_Layouts( $this->content, $this->settings );
 	}
@@ -56,6 +56,63 @@ JSON;
 
 		$this->assertEquals( 'banner_advertisement', $json['role'] );
 		$this->assertEquals( 'double_height', $json['bannerType'] );
+	}
+
+	public function testJSONSaveValidTokens() {
+		$json = <<<JSON
+{
+    "role": "audio",
+    "URL": "http://someurl.com"
+}
+JSON;
+		$nonce = wp_create_nonce( 'apple_news_json' );
+		$_POST['apple_news_component'] = 'Audio';
+		$_POST['apple_news_action'] = 'apple_news_save_json';
+		$_POST['apple_news_json_json'] = $json;
+		$_POST['page'] = 'apple-news-json';
+		$_POST['redirect'] = false;
+		$_REQUEST['_wp_http_referer'] = '/wp-admin/admin.php?page=apple-news-json';
+		$_REQUEST['_wpnonce'] = $nonce;
+
+		$admin_json = new \Admin_Apple_JSON();
+		$admin_json->action_router();
+
+		$stored_json_specs = get_option( 'apple_news_json_audio' );
+		$stored_json = stripslashes( json_encode( $stored_json_specs['apple_news_json_json'], JSON_PRETTY_PRINT ) );
+		$this->assertEquals( $stored_json, $json );
+	}
+
+	public function testJSONSaveInvalidTokens() {
+		$invalid_json = <<<JSON
+{
+    "role": "audio",
+    "URL": "%%invalid%%"
+}
+JSON;
+		$nonce = wp_create_nonce( 'apple_news_json' );
+		$_POST['apple_news_component'] = 'Audio';
+		$_POST['apple_news_action'] = 'apple_news_save_json';
+		$_POST['apple_news_json_json'] = $invalid_json;
+		$_POST['page'] = 'apple-news-json';
+		$_POST['redirect'] = false;
+		$_REQUEST['_wp_http_referer'] = '/wp-admin/admin.php?page=apple-news-json';
+		$_REQUEST['_wpnonce'] = $nonce;
+
+		$admin_json = new \Admin_Apple_JSON();
+		$admin_json->action_router();
+
+		$stored_json_specs = get_option( 'apple_news_json_audio' );
+		$stored_json = json_encode( $stored_json_specs['apple_news_json_json'], JSON_PRETTY_PRINT );
+
+		// Pass the mock workspace as a dependency
+		$workspace = $this->prophet->prophesize( '\Exporter\Workspace' );
+		$audio = new Audio( '<audio><source src="http://someurl.com/audio-file.mp3?some_query=string"></audio>',
+			$workspace->reveal(), $this->settings, $this->styles, $this->layouts );
+		$json = $audio->to_array();
+		$specs = $audio->get_specs();
+
+		$this->assertEquals( $stored_json_specs, '' );
+		$this->assertEquals( $specs['json']->get_spec(), $specs['json']->spec );
 	}
 }
 
