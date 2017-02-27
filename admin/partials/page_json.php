823e43d2f52d703ea8f2ff47de78484f5c1f7723
bcampeau <brad@alleyinteractive.com>
2017-02-27 14:46:33 -0500

    Updated to more aesthetically similar theme

--- admin/partials/page_json.php
+++ admin/partials/page_json.php
@@ -58,7 +58,7 @@
 							var <?php echo esc_js( $editor_name ) ?> = ace.edit( '<?php echo esc_js( $editor_name ) ?>' );
 							jQuery( function() {
 								jQuery( '#<?php echo esc_js( $field_name ) ?>' ).hide();
-								<?php echo esc_js( $editor_name ) ?>.setTheme( 'ace/theme/twilight' );
+								<?php echo esc_js( $editor_name ) ?>.setTheme( 'ace/theme/textmate' );
 								<?php echo esc_js( $editor_name ) ?>.getSession().setMode( 'ace/mode/json' );
 								<?php echo esc_js( $editor_name ) ?>.getSession().setTabSize( 2 );
 								<?php echo esc_js( $editor_name ) ?>.getSession().setUseSoftTabs( false );
