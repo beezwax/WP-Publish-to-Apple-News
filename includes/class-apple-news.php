956bd906d5541cc200e0325559ad09d76b0a05c2
bcampeau <brad@alleyinteractive.com>
2017-02-28 13:14:51 -0500

    New screenshots for wiki, prepping 1.2.4 release

--- includes/class-apple-news.php
+++ includes/class-apple-news.php
@@ -39,7 +39,7 @@ class Apple_News {
 	 * @var string
 	 * @access public
 	 */
-	public static $version = '1.2.3';
+	public static $version = '1.2.4';
 
 	/**
 	 * Link to support for the plugin on WordPress.org.
