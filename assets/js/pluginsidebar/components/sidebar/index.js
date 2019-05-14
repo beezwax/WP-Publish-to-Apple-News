d866a7cd1c3cb963e5b9aec25971d05d91aef00c
Dan Bowles <dbowles@alleyinterative.com>
2019-05-14 14:58:51 -0400

    adding more meta fields

--- assets/js/pluginsidebar/components/sidebar/index.js
+++ assets/js/pluginsidebar/components/sidebar/index.js
@@ -8,8 +8,9 @@ const {
   },
   components: {
     PanelBody,
+    CheckboxControl,
+    SelectControl,
     TextareaControl,
-    TextControl,
   },
   data: {
     withDispatch,
@@ -58,26 +59,91 @@ class Sidebar extends React.PureComponent {
         name="publish-to-apple-news"
         title={__('Publish to Apple News Options', 'publish-to-apple-news')}
       >
+        <div className="components-panel__body is-opened">
+          <h3>Sections</h3>
+          <CheckboxControl
+            label="Main"
+          />
+          <CheckboxControl
+            label="Animals"
+          />
+          <CheckboxControl
+            label="Food"
+          />
+          <p>
+            <em>
+              Select the sections in which to publish this article.&nbsp;
+              If none are selected, it will be published to the default section.
+            </em>
+          </p>
+          <h3>Preview Article</h3>
+          <CheckboxControl
+            label="Check this to publish the article as a draft."
+          />
+          <h3>Hidden Article</h3>
+          <CheckboxControl
+            // eslint-disable-next-line max-len
+            label="Hidden articles are visible to users who have a link to the article, but do not appear in feeds."
+          />
+          <h3>Sponsored Article</h3>
+          <CheckboxControl
+            label="Check this to indicate this article is sponsored content."
+          />
+        </div>
         <PanelBody
           initialOpen={false}
-          title={__('Open Graph', 'kauffman')}
+          title={__('Maturity Rating', 'kauffman')}
         >
-          <TextControl
-            label={__('Title', 'kauffman')}
-            onChange={(value) => onUpdate(
-              'kauffman_open_graph_title',
-              value
-            )}
-            value="Title"
+          <SelectControl
+            label="Select Maturity Rating"
+            value=""
+            options={[
+              { label: '', value: '' },
+              { label: 'Kids', value: 'KIDS' },
+              { label: 'Mature', value: 'MATURE' },
+              { label: 'General', value: 'GENERAL' },
+            ]}
           />
+          <p>
+            <em>
+              Select the optional maturity rating for this post.
+            </em>
+          </p>
+        </PanelBody>
+        <PanelBody
+          initialOpen={false}
+          title={__('Pull Quote', 'kauffman')}
+        >
           <TextareaControl
             label={__('Description', 'kauffman')}
-            onChange={(value) => onUpdate(
-              'kauffman_open_graph_description',
-              value
-            )}
-            value="Sup"
+            // eslint-disable-next-line max-len
+            placeholder="A pull quote is a key phrase, quotation, or excerpt that has been pulled from an article and used as a graphic element, serving to entice readers into the article or to highlight a key topic."
           />
+          <p>
+            <em>
+              This is optional and can be left blank.
+            </em>
+          </p>
+          <SelectControl
+            label="Pull Quote Position"
+            value="middle"
+            options={[
+              { label: 'top', value: 'top' },
+              { label: 'middle', value: 'middle' },
+              { label: 'bottom', value: 'bottom' },
+            ]}
+          />
+          <p>
+            <em>
+              The position in the article where the pull quote will appear.
+            </em>
+          </p>
+        </PanelBody>
+        <PanelBody
+          initialOpen={false}
+          title={__('Cover Art', 'kauffman')}
+        >
+          Hi
         </PanelBody>
       </PluginSidebar>
     );
