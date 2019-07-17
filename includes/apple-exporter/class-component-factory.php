865b70b25bac2281256213a00ca0f8f618a03c01
Dan Bowles <dbowles@alley.co>
2019-07-17 09:42:14 -0400

    removing generic iframe component for now

--- includes/apple-exporter/class-component-factory.php
+++ includes/apple-exporter/class-component-factory.php
@@ -93,7 +93,6 @@ class Component_Factory {
 		self::register_component( 'facebook', '\\Apple_Exporter\\Components\\Facebook' );
 		self::register_component( 'instagram', '\\Apple_Exporter\\Components\\Instagram' );
 		self::register_component( 'table', '\\Apple_Exporter\\Components\\Table' );
-		self::register_component( 'iframe-generic', '\\Apple_Exporter\\Components\\Iframe_Generic' );
 		self::register_component( 'flickr', '\\Apple_Exporter\\Components\\Flickr' );
 		self::register_component( 'iframe', '\\Apple_Exporter\\Components\\Embed_Web_Video' );
 		self::register_component( 'img', '\\Apple_Exporter\\Components\\Image' );
