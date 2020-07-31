a67903fce54101907279a8ea93083b6803e25221
kevinfodness <kevin@kevinfodness.com>
2020-07-31 11:42:50 -0400

    APPLE-40 Add changelog entry and settings page info for Brightcove support

--- admin/partials/page-options.php
+++ admin/partials/page-options.php
@@ -23,6 +23,18 @@
 			?>
 		<?php endforeach; ?>
 
+		<?php if ( is_plugin_active( 'brightcove-video-connect/brightcove-video-connect.php' ) ) : ?>
+			<h3><?php esc_html_e( 'Brightcove Support', 'apple-news' ); ?></h3>
+			<p>
+				<?php
+					esc_html_e(
+						'Brightcove support was added in version 2.1.0 for users of the Brightcove Video Connect plugin. However, you will need to contact Apple Support to connect your Brightcove account to your Apple News channel for this feature to work properly.',
+						'apple-news'
+					);
+				?>
+			</p>
+		<?php endif; ?>
+
 		<?php submit_button(); ?>
 	</form>
 </div>
