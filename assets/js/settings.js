6fd1bc964020f777eef1d00072ea90a0831e5b82
tylermachado <tmachado@alley.co>
2021-07-04 11:42:31 -0400

    setting regexp strings

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -20,10 +20,32 @@
         const file = e.target.files[0];
         const reader = new FileReader();
         reader.onload = function(f) {
-          $( '#api_config_file_input' ).text(f.target.result);
+          // When a file is uploaded, the read contents populate the hidden textarea.
+          $( '#api_config_file_input' ).val(f.target.result);
         };
         reader.readAsText(file);
       }
 		} );
+
+    // Reading in the needed values from the hidden textarea field.
+    $( '#api_config_file_input' ).on( 'change', function () {
+      var input = $( '#api_config_file_input' ).val();
+
+      var RegExpStrings = {
+        channel_id: /channel_id: [0-9a-zA-Z_-]*/g,
+        key: /key: [0-9a-zA-Z_-]*/g,
+        secret: /secret: [0-9a-zA-Z_-]*/g
+      }
+
+      $( '#api_channel' ).val(
+        input.match(RegExpStrings.channel_id).toString().split(" ")[1]
+      );
+      $( '#api_key' ).val(
+        input.match(RegExpStrings.key).toString().split(" ")[1]
+      );
+      $( '#api_secret' ).val(
+        input.match(RegExpStrings.secret).toString().split(" ")[1]
+      );
+    });
 	}
 )( jQuery, window );
