d0d2c05af2ac51c30f82c7705a2054c1f1f25e66
tylermachado <tmachado@alley.co>
2021-07-06 11:16:35 -0400

    better labels

--- admin/settings/class-admin-apple-settings-section-api.php
+++ admin/settings/class-admin-apple-settings-section-api.php
@@ -33,11 +33,10 @@ class Admin_Apple_Settings_Section_API extends Admin_Apple_Settings_Section {
 		// Add the settings.
 		$this->settings = array(
 			'api_config_file'     => array(
-				'label' => __( 'Upload channel configuration file:', 'apple-news' ),
 				'type'  => 'file',
 			),
       'api_config_file_input'     => array(
-				'label' => __( 'PAPI File Input', 'apple-news' ),
+				'label' => __( 'Advanced users <a href="#">click here</a>.', 'apple-news' ),
 				'type'  => 'textarea',
 			),
 			'api_channel'         => array(
@@ -70,9 +69,13 @@ class Admin_Apple_Settings_Section_API extends Admin_Apple_Settings_Section {
 
 		// Add the groups.
 		$this->groups = array(
-			'apple_news' => array(
-				'label'    => __( 'Apple News API', 'apple-news' ),
-				'settings' => array( 'api_config_file', 'api_config_file_input', 'api_channel', 'api_key', 'api_secret', 'api_autosync', 'api_autosync_update', 'api_autosync_delete', 'api_async' ),
+			'apple_news_config_upload' => array(
+				'label'    => __( 'Upload Channel Configuration File:', 'apple-news' ),
+				'settings' => array( 'api_config_file', 'api_config_file_input', 'api_channel', 'api_key', 'api_secret'),
+			),
+      'apple_news_options' => array(
+				'label'    => __( 'Apple News API Options', 'apple-news' ),
+				'settings' => array( 'api_autosync', 'api_autosync_update', 'api_autosync_delete', 'api_async' ),
 			),
 		);
 
