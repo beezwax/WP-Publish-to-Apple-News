a32bf3d3fdd03cbe916ae7becec95435fb4578b8
kevinfodness <2650828+kevinfodness@users.noreply.github.com>
2021-10-22 17:13:39 -0400

    APPLE-115 WIP fix video embeds

--- tests/apple-exporter/components/test-class-embed-web-video.php
+++ tests/apple-exporter/components/test-class-embed-web-video.php
@@ -6,8 +6,6 @@
  * @subpackage Tests
  */
 
-use Apple_Exporter\Components\Embed_Web_Video;
-
 /**
  * A class to test the behavior of the
  * Apple_Exporter\Components\Embed_Web_Video class.
@@ -15,104 +13,138 @@ use Apple_Exporter\Components\Embed_Web_Video;
  * @package Apple_News
  * @subpackage Tests
  */
-class Embed_Web_Video_Test extends Component_TestCase {
+class Embed_Web_Video_Test extends Apple_News_Testcase {
 
 	/**
-	 * A data provider for the testTransformEmbedWebVideo function.
+	 * A data provider for the test_transform function.
 	 *
-	 * @see self::testTransformEmbedWebVideo()
-	 *
-	 * @access public
-	 * @return array Parameters to use when calling testTransformEmbedWebVideo.
+	 * @return array An array of arrays representing function arguments.
 	 */
-	public function dataTransformEmbedWebVideo() {
-		return array(
-			array(
-				'<p>https://www.youtube.com/embed/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>https://www.youtube.com/watch?v=0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>https://youtube.com/embed/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>https://youtube.com/watch?v=0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>http://www.youtube.com/embed/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>http://www.youtube.com/watch?v=0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>http://youtube.com/embed/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>http://youtu.be/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>http://youtube.com/watch?v=0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>https://youtu.be/0qwALOOvUik</p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p>https://www.youtube.com/watch?v=J2nN-Yrt1EU</p>',
-				'https://www.youtube.com/embed/J2nN-Yrt1EU',
-			),
-			array(
-				'<p>https://www.youtube.com/embed/J2nN-Yrt1EU</p>',
-				'https://www.youtube.com/embed/J2nN-Yrt1EU',
-			),
-			array(
-				'<p>https://youtu.be/J2nN-Yrt1EU</p>',
-				'https://www.youtube.com/embed/J2nN-Yrt1EU',
-			),
-			array(
-				'<p>https://vimeo.com/12819723</p>',
+	public function data_transform() {
+		return [
+			'Vimeo standard watch URL'           => [
+				'vimeo',
+				'https://vimeo.com/12819723',
 				'https://player.vimeo.com/video/12819723',
-			),
-			array(
-				'<iframe title="YouTube" width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik" frameborder="0" allowfullscreen></iframe>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<iframe title="YouTube" width="640" height="360" src="https://www.youtube.com/embed/0qwALOOvUik" frameborder="0" allowfullscreen></iframe>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<iframe width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik?autoplay=1"></iframe>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<iframe width="640" height="360" src="http://www.youtube.com/embed/0qwALOOvUik" />',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<p><a href="http://youtube.com/embed/0qwALOOvUik">http://youtube.com/embed/0qwALOOvUik</a></p>',
-				'https://www.youtube.com/embed/0qwALOOvUik',
-			),
-			array(
-				'<iframe src="//player.vimeo.com/video/12819723" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+			],
+			'Vimeo standard watch URL, no https' => [
+				'vimeo',
+				'http://vimeo.com/12819723',
 				'https://player.vimeo.com/video/12819723',
-			),
-			array(
-				'<iframe src="//player.vimeo.com/video/12819723?title=0&byline=0&portrait=0" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
-				'https://player.vimeo.com/video/12819723',
-			),
-		);
+			],
+			'YouTube standard watch URL'                   => [
+				'youtube',
+				'https://www.youtube.com/watch?v=0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube embed URL'                            => [
+				'youtube',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube standard watch URL, no www'           => [
+				'youtube',
+				'https://youtube.com/watch?v=0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube embed URL, no www'                    => [
+				'youtube',
+				'https://youtube.com/embed/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube standard watch URL, no https'         => [
+				'youtube',
+				'http://www.youtube.com/watch?v=0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube embed URL, no https'                  => [
+				'youtube',
+				'http://www.youtube.com/embed/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube standard watch URL, no https, no www' => [
+				'youtube',
+				'http://youtube.com/watch?v=0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube embed URL, no https, no www'          => [
+				'youtube',
+				'http://youtube.com/embed/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube shortlink'                            => [
+				'youtube',
+				'https://youtu.be/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+			'YouTube shortlink, no https'                  => [
+				'youtube',
+				'http://youtu.be/0qwALOOvUik',
+				'https://www.youtube.com/embed/0qwALOOvUik',
+			],
+		];
 	}
+
+	/**
+	 * Tests transforming an embed to an Embed Web Video component.
+	 *
+	 * @dataProvider data_transform
+	 *
+	 * @param string $provider  The provider. One of 'youtube' or 'vimeo'.
+	 * @param string $embed_url The user-entered oEmbed URL.
+	 * @param string $expected  The expected embed URL that is rendered in the component.
+	 */
+	public function test_transform( $provider, $embed_url, $expected ) {
+		// Test in Gutenberg.
+		$post_content = <<<HTML
+<!-- wp:embed {"url":"{$embed_url}","type":"video","providerNameSlug":"{$provider}","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-{$provider} wp-block-embed-{$provider} wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+		{$embed_url}
+</div></figure>
+<!-- /wp:embed -->
+HTML;
+		$post_id      = self::factory()->post->create( [ 'post_content' => $post_content ] );
+		$json         = $this->get_json_for_post( $post_id );
+		$this->assertEquals( $expected, $json['components'][3]['URL'] );
+
+		// Test in the classic editor.
+		$post_content = <<<HTML
+<p>{$embed_url}</p>
+HTML;
+		$post_id      = self::factory()->post->create( [ 'post_content' => $post_content ] );
+		$json         = $this->get_json_for_post( $post_id );
+		$this->assertEquals( $expected, $json['components'][3]['URL'] );
+	}
+
+	/**
+	 * Tests the transformation of an iframe containing a Vimeo embed into an
+	 * EmbedWebVideo component.
+	 */
+	public function test_transform_vimeo_iframe() {
+		$this->become_admin();
+		$post_content = <<<HTML
+<iframe title="vimeo-player" src="https://player.vimeo.com/video/12819723?h=eafba1f705" width="640" height="360" frameborder="0" allowfullscreen></iframe>
+HTML;
+		$post_id      = self::factory()->post->create( [ 'post_content' => $post_content ] );
+		$json         = $this->get_json_for_post( $post_id );
+		$this->assertEquals( 'https://player.vimeo.com/video/12819723', $json['components'][3]['URL'] );
+	}
+
+	/**
+	 * Tests the transformation of an iframe containing a YouTube embed into an
+	 * EmbedWebVideo component.
+	 */
+	public function test_transform_youtube_iframe() {
+		$this->become_admin();
+		$post_content = <<<HTML
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0qwALOOvUik" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+HTML;
+		$post_id      = self::factory()->post->create( [ 'post_content' => $post_content ] );
+		$json         = $this->get_json_for_post( $post_id );
+		$this->assertEquals( 'https://www.youtube.com/embed/0qwALOOvUik', $json['components'][3]['URL'] );
+	}
+
+	// TODO: REFACTOR LINE
 
 	/**
 	 * A filter function to modify the aspect ratio.
@@ -124,6 +156,7 @@ class Embed_Web_Video_Test extends Component_TestCase {
 	 */
 	public function filter_apple_news_embed_web_video_json( $json ) {
 		$json['aspectRatio'] = '1.4';
+
 		return $json;
 	}
 
@@ -174,7 +207,7 @@ class Embed_Web_Video_Test extends Component_TestCase {
 	 *
 	 * @dataProvider dataTransformEmbedWebVideo
 	 *
-	 * @param string $html      The HTML to be matched by the parser.
+	 * @param string $html The HTML to be matched by the parser.
 	 * @param string $final_url The final URL used in the JSON.
 	 *
 	 * @access public
