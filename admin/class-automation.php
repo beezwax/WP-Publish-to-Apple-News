c04d57796584a6852db6f686fc5e14397ae7faf1
bh247484 <bh247484@gmail.com>
2022-11-15 15:28:13 -0500

    Remove old settings page code, register settings, begin React hook/form work

--- admin/class-automation.php
+++ admin/class-automation.php
@@ -59,7 +59,7 @@ class Automation {
 	 * Initialize functionality of this class by registering hooks.
 	 */
 	public static function init(): void {
-		add_action( 'admin_init', [ __CLASS__, 'action__admin_init' ] );
+		add_action( 'init', [ __CLASS__, 'action__admin_init' ] );
 		add_action( 'admin_menu', [ __CLASS__, 'action__admin_menu' ] );
 		add_filter( 'apple_news_article_metadata', [ __CLASS__, 'filter__apple_news_article_metadata' ], 0, 2 );
 	}
@@ -68,27 +68,17 @@ class Automation {
 	 * A callback function for the admin_init action hook.
 	 */
 	public static function action__admin_init(): void {
-		add_settings_section(
-			self::PAGE_NAME,
-			esc_html__( 'Configuration', 'apple-news' ),
-			'__return_null',
-			self::PAGE_NAME
-		);
-		add_settings_field(
-			self::FIELD_NAME,
-			esc_html__( 'By Taxonomy Term', 'apple-news' ),
-			[ __CLASS__, 'render_settings_field' ],
-			self::PAGE_NAME,
-			self::PAGE_NAME
-		);
+		// New React Routine
 		register_setting(
 			self::PAGE_NAME,
 			self::OPTION_KEY,
 			[
-				'default'           => [],
+				'default'           => 'default',
 				'description'       => __( 'Automation settings for Publish to Apple News.', 'apple-news' ),
 				'sanitize_callback' => [ __CLASS__, 'sanitize_setting' ],
-				'type'              => 'array',
+				// Do we need a schema here?
+				'show_in_rest'      => true,
+				'type'              => 'string',
 			]
 		);
 	}
@@ -105,16 +95,6 @@ class Automation {
 			apply_filters( 'apple_news_settings_capability', 'manage_options' ),
 			self::PAGE_NAME,
 			[ __CLASS__, 'render_submenu_page' ]
-		);
-
-		add_submenu_page(
-			'apple_news_index',
-			__( 'Apple News React Options', 'apple-news' ),
-			__( 'React Options', 'apple-news' ),
-			/** This filter is documented in admin/class-admin-apple-settings.php */
-			apply_filters( 'apple_news_settings_capability', 'manage_options' ),
-			'apple-news-react-options',
-			[ __CLASS__, 'render_react_submenu_page' ]
 		);
 
 		add_action( "admin_print_scripts", [ __CLASS__, 'add_options_assets' ] );
@@ -182,74 +162,9 @@ class Automation {
 	}
 
 	/**
-	 * A render callback for the automation settings field.
-	 */
-	public static function render_settings_field(): void {
-		$value = self::get_automation_rules();
-		// TODO: Dynamically get list of registered taxonomies.
-		?>
-			<fieldset>
-				<legend class="screen-reader-text">
-					<?php esc_html_e( 'Configuration', 'apple-news' ); ?> 1
-				</legend>
-				<div>
-					<label>
-						<?php esc_html_e( 'Taxonomy', 'apple-news' ); ?>
-						<select name="<?php echo esc_attr( self::OPTION_KEY ); ?>[0][taxonomy]">
-							<option value=""></option>
-							<option value="category" <?php selected( 'category' === ( $value[0]['taxonomy'] ?? '' ) ); ?>><?php esc_html_e( 'Category', 'apple-news' ); ?></option>
-							<option value="post_tag" <?php selected( 'post_tag' === ( $value[0]['taxonomy'] ?? '' ) ); ?>><?php esc_html_e( 'Tag', 'apple-news' ); ?></option>
-						</select>
-					</label>
-				</div>
-				<div>
-					<label>
-						<?php esc_html_e( 'Term ID', 'apple-news' ); ?>
-						<input name="<?php echo esc_attr( self::OPTION_KEY ); ?>[0][term_id]" type="number" value="<?php echo esc_attr( $value[0]['term_id'] ?? '' ); ?>" />
-					</label>
-				</div>
-				<div>
-					<label>
-						<?php esc_html_e( 'Field', 'apple-news' ); ?>
-						<select name="<?php echo esc_attr( self::OPTION_KEY ); ?>[0][field]">
-							<option value=""></option>
-							<?php foreach ( array_keys( self::FIELDS ) as $field_key ) : ?>
-								<option value="<?php echo esc_attr( $field_key ); ?>" <?php selected( ( $value[0]['field'] ?? '' ) === $field_key ); ?>><?php echo esc_html( $field_key ); ?></option>
-							<?php endforeach; ?>
-						</select>
-					</label>
-				</div>
-				<div>
-					<label>
-						<?php esc_html_e( 'Field Value', 'apple-news' ); ?>
-						<input name="<?php echo esc_attr( self::OPTION_KEY ); ?>[0][value]" type="text" value="<?php echo esc_attr( $value[0]['value'] ?? '' ); ?>" />
-					</label>
-				</div>
-			</fieldset>
-		<?php
-	}
-
-	/**
-	 * A render callback for the submenu page.
-	 */
-	public static function render_submenu_page(): void {
-		?>
-			<div class="wrap apple-news-settings">
-				<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-				<?php settings_errors(); ?>
-				<form method="post" action="options.php" id="apple-news-automation">
-					<?php settings_fields( 'apple-news-automation' ); ?>
-					<?php do_settings_sections( 'apple-news-automation' ); ?>
-					<?php submit_button(); ?>
-				</form>
-			</div>
-		<?php
-	}
-
-	/**
 	 * A render callback for the REACT submenu page.
 	 */
-	public static function render_react_submenu_page(): void {
+	public static function render_submenu_page(): void {
 		add_filter(
 			'should_load_block_editor_scripts_and_styles',
 			function() {
@@ -272,6 +187,8 @@ class Automation {
 			true
 		);
 		wp_enqueue_style( 'wp-edit-blocks' );
+		// Was included in BASS logic, method not defined here.
+		// Leaving for now in case it's important and needs to be done differently here.
 		// inline_locale_data( 'apple-news-plugin-admin-settings' );
 	}
 
--- assets/js/admin-settings/index.jsx
+++ assets/js/admin-settings/index.jsx
@@ -5,13 +5,47 @@ import {
   ToggleControl,
 } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
-import React from 'react';
+import React, { useState } from 'react';
+import useSiteOptions from '../services/hooks/use-site-options';
 
 
 const AdminSettings = () => {
+  const [{ loading, saving, settings }, setOptions] = useSiteOptions();
+  const [test, setTest] = useState('');
+
+  /**
+   * Helper function for saving the in-memory settings to the server.
+   */
+   const saveSettings = () => {
+    const next = { ...settings, apple_news_automation: test };
+
+    // Enforce some defaults prior to save.
+    next.site_logo = next.site_logo ?? 0;
+    console.log('settings-pre-save', next);
+    console.log('test');
+
+    // Kick off the save to the server.
+    setOptions(next);
+  };
+
+  console.log(settings);
   return (
     <div className="apple-news-options__wrapper">
-      Hello From React
+      <TextControl
+        disabled={loading || saving}
+        label={__('Test', 'bassmaster-plugin')}
+        onChange={(next) => setTest(next)}
+        type="text"
+        value={test}
+      />
+
+      <Button
+        disabled={loading || saving}
+        isPrimary
+        onClick={saveSettings}
+      >
+        {__('Save Settings', 'apple-news-plugin')}
+      </Button>
     </div>
   );
 };
