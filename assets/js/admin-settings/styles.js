199552289a2a96a3d420402e6fa7473cfa6ae1c4
bh247484 <bh247484@gmail.com>
2022-11-17 14:03:15 -0500

    Add flex gap between rule cards

--- assets/js/admin-settings/styles.js
+++ assets/js/admin-settings/styles.js
@@ -1,9 +1,10 @@
 export const ruleCard = {
   width: '250px',
   border: '2px solid black',
-  padding: '5px'
+  padding: '5px',
 }
 
 export const ruleCorral = {
   display: 'flex',
+  gap: '20px',
 }
